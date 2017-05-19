864772d8b4d23e269ab1d32874d3fbb416488be8
Diko Tech Slave <diko316@gmail.com>
2017-05-19 09:10:50 +0800

    implemented load() fsm data import  unit test.

--- src/test/api/load.js
+++ src/test/api/load.js
@@ -3,12 +3,58 @@
 describe('Loads LALR pushdown FSM data by calling ' +
     'load(fsmData:{String|Object}) method',
     function () {
+        var exported;
+
+        beforeEach(function () {
+            var parser = global.main.define("Expr",
+                        [
+                            "Expr", [
+                                        "Add"
+                                    ],
+                            
+                            "Add",  [
+                                        ["Add", /\+/, "Mul"],
+                                        "Mul",
+                                    ],
+                            "Mul",  [
+                                        ["Mul", /\*/, "Unit"],
+                                        "Unit"
+                                    ],
+                            "Unit", [
+                                        "Number",
+                                        [/\(/, "Expr", /\)/]
+                                    ],
+                            "Number", [
+                                        /(\+|\-)?[0-9]+(\.[0-9]+)?/
+                                    ]
+                        ],
+                        [
+                            /[ \r\n\t]+/
+                        ]);
+            exported = parser.toJSON();
+        });
+        
         
         it('1. Should load fsm data from JSON String parameter.',
             function () {
+                var param = exported;
+                
+                function load() {
+                    return global.main.load(param);
+                }
+                
+                expect(load).not.toThrow();
+                
             });
         
         it('2. Should load fsm data from Object parameter.',
             function () {
+                var param = exported;
+                
+                function load() {
+                    return global.main.load(JSON.parse(param));
+                }
+                
+                expect(load).not.toThrow();
             });
     });
